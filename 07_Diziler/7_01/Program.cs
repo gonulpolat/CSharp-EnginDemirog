c8d0d815684763796dc9079a0836b66fea5c389e
gonulpolat <gp29polat@gmail.com>
2025-01-21 20:44:19 +0300

    Diziler

---
+++ 07_Diziler/7_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array kullanmadan
+            //string student1 = "Gamze";
+            //string student2 = "Ela";
+            //string student3 = "Sena";
+
+            // 1. Way:
+            string[] students = new string[3];
+            students[0] = "Gamze";
+            students[1] = "Ela";
+            students[2] = "Sena";
+
+            // 2. Way:
+            string[] students1 = new [] {"Gamze", "Ela", "Sena" };
+
+            // 3. Way:
+            string[] students2 = { "Gamze", "Ela", "Sena" };
+
+            // 4. Way:
+            string[] students3 = new string[3]{ "Gamze", "Ela", "Sena" };
+
+
+            foreach (string s in students3)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 07_Diziler/7_02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Çok Boyutlu Diziler (multidimensional)
+
+            string[,] regions = new string[7, 3] {  // 7 satır 3 kolon
+                {"İstanbul", "Çanakkale", "Tekirdağ"},
+                {"Manisa", "Aydın", "Denizli"},
+                {"Ankara", "Konya", "Çankırı"},
+                {"Samsun", "Sinop", "Tokat"},
+                {"Van", "Erzurum", "Ağrı"},
+                {"Mardin", "Diyarbakır", "Hakkari"},
+                {"Antalya", "Hatay", "Adana"}
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i,j]);
+                }
+                Console.WriteLine("------------------------");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
